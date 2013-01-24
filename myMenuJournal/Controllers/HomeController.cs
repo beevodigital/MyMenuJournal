0af840977768f66bee55309244bf498cf425a259
Brian Evans <brian.evans@space150.com>
2013-01-24 09:07:57 -0600

    initial commit

---
+++ myMenuJournal/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace myMenuJournal.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
