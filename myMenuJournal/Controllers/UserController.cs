97c4a8a1efee6f3c18aa292daafe8dd3bb7b451c
Brian Evans <brian.evans@space150.com>
2013-02-12 08:31:57 -0600

    latest

---
+++ myMenuJournal/Controllers/UserController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace myMenuJournal.Controllers
+{
+    public class UserController : Controller
+    {
+        //
+        // GET: /User/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult FacebookLogin()
+        {
+            string stopHere = "";
+            Membership.CreateUser("bewebdev", "one5four","bewebdev@gmail.com");
+            return Redirect("/");
+        }
+
+    }
+}
